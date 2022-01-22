943a1c5dc6dbbc81f169712814d80c890c4104c2
gucchisk <gucchi_sk@yahoo.co.jp>
2022-01-23 02:49:27 +0900

    Add revision

--- Formula/anaguma.rb
+++ Formula/anaguma.rb
@@ -4,6 +4,7 @@ class Anaguma < Formula
   desc "anaguma is CLI for badagerDB"
   url "https://github.com/gucchisk/anaguma.git", :tag => "v#{VERSION}"
   version VERSION
+  revision 1
 
   depends_on "go" => :build
 
