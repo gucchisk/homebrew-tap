aa79c46df13998d661ab734e4c5f0651e396e0af
gucchisk <gucchi_sk@yahoo.co.jp>
2026-05-11 09:12:05 +0900

    fix: build

--- Formula/exradix-cli.rb
+++ Formula/exradix-cli.rb
@@ -4,11 +4,23 @@ class ExradixCli < Formula
   url "https://github.com/gucchisk/exradix-cli.git", branch: "main"
   version "0.1.0"
 
+  resource "exradix" do
+    url "ssh://git@github.com/gucchisk/exradix.git", branch: "main"
+  end
+
   depends_on "cmake" => :build
   
   def install
     cd buildpath do
-      system "cmake . && make"
+      (buildpath/"exradix").install resource("exradix")
+      args = std_cmake_args + %W[
+        # 3. FetchContent がネットワークアクセスしないよう完全遮断
+        -DFETCHCONTENT_FULLY_DISCONNECTED=ON
+        # 4. FetchContent_Declare("exradix" ...) の取得先をローカルに差し替え
+        -DFETCHCONTENT_SOURCE_DIR_EXRADIX=#{buildpath}/exradix
+      ]
+      system "cmake",  ".", *args
+      system "make"
       bin.install "exradix-cli" => "exradix-cli"
     end
   end
